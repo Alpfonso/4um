9112c82acc2433d2958ed317d5f4bbde3f0eb9a9
Alpfonso <lukasilov1@gmail.com>
2021-05-12 11:38:33 +0200

    Updated pages for keyword extraction; saved migrations

--- Forum/pages/views.py
+++ Forum/pages/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.contrib.auth import login, authenticate
 
 from django.contrib.auth.forms import UserCreationForm # Import RenderForm from forms.py
@@ -9,28 +10,67 @@ from forum_threads.forms import ForumForm
 from forum_threads.models import Thread
 from django.core.paginator import Paginator
 
+from .models import Profile
+from forum_threads import keyword_extraction
+
+
 # Create your views here.
+from django.db.models import Q
 
 def home_view(request, *args, **kwargs):
+    vote_points = Profile(username=request.user.username)
     queryset = Thread.objects.all()
-    paginator = Paginator(queryset, 20)
+    if request.GET.get('tag'):
+        queryset = Thread.objects.filter(Q(tags__contains=request.GET.get('tag'))) # Check for threads/tags that contain given string
+    paginator = Paginator(queryset, 9)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
-    return render(request, "home.html", {'page_obj': page_obj})
+    context = {
+        'page_obj': page_obj,
+        'vote':vote_points
+    }
+    return render(request, "home.html", context)
 
 def profile_view(request, *args, **kwargs):
+    user = request.GET.get('user')
+    #userQuerry = Users.objects.filter(username=user)
+    if user == request.user.username:
+        print("yoyo")
+    else:
+        print("nono")
+    profileQuery = Profile.objects.filter(username=user)
+    print(profileQuery)
     my_context = {
-        "my_text": "This is about me",
-        "my_number": 123,
-        "list": [1,2,3],
+        "profile":profileQuery
     }
     return render(request, "profile.html", my_context)
 
 def new_post_view(request, *args, **kwargs):
     form = ForumForm(request.POST, user=request.user)
+    extracted_tags = None
+    
+    confirmation = request.POST.get('radio_tag')
+    
+    
+
     if form.is_valid():
-        form.save()
-        form = ForumForm(user=request.user)
+        
+        model_instance = form.save()
+        if confirmation == 'True':
+            
+            #print("<<--Keyword extraction enabled-->>")
+            obj = Thread.objects.get(id=model_instance.id)
+            extracted_tags = keyword_extraction.initialize(model_instance.description, True)
+            keyword_extraction.textrank(model_instance.description)
+            obj.set_tags(extracted_tags)
+            obj.save()
+
+            profile_obj = Profile.objects.get(username=request.user.username)
+            profile_obj.set_tags(extracted_tags)
+            profile_obj.save()
+            return redirect("/details?id="+str(model_instance.id))
+            #form = ForumForm(request.POST, user=request.user)
+        #form = ForumForm(user=request.user)
         return redirect("/")
     return render(request, "new_post.html", {'form': form})
 
@@ -43,6 +83,8 @@ def register_view(request):
         # check whether it's valid:
         if form.is_valid():
             form.save()
+            profile = Profile(username = form.cleaned_data.get('username'))
+            profile.save()
             username = form.cleaned_data.get('username')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=raw_password)
